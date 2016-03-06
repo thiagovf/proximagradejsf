b0c6593482d923b99e2b3c572d11cef66d9d39d5
thiagovf <thiagu.vf@gmail.com>
2016-03-06 10:58:34 -0300

    testing

--- src/main/java/br/com/equipejr/entity/User.java
+++ src/main/java/br/com/equipejr/entity/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -17,7 +18,7 @@ import javax.persistence.Table;
 public class User {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@Column(nullable = false)
